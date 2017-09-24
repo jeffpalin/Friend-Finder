a6dbf8a1db110465e189863f71fed61f4aaf37f7
Jeff Palin <jeffpalin@gmail.com>
2017-09-24 10:03:23 -0700

    periodic commit

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,26 @@
-//
+// ===============================================================================
+// DATA
+// Below data will hold all of the friends info.
+// Initially we just set it equal to a "dummy" friend.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var friendsArray = {
+    "name": "Steph",
+    "photo": "https://espnfivethirtyeight.files.wordpress.com/2017/04/mvp-curry-4x3.png?w=1200",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+};
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,1 +1,49 @@
-//
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a "data" source.
+// This data source holds an array of information on friends.
+// ===============================================================================
+
+var friendsData = require("../data/friends");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+    // API GET Request
+    // Below code handles when users "visit" a page.
+    // When a user visits a link (ex: localhost:PORT/api/admin... they are shown a
+    // JSON of the data in the table)
+    // ---------------------------------------------------------------------------
+
+    app.get("/api/friends", function(req, res) {
+        res.json(friendsData);
+    });
+
+    // API POST Requests
+    // Below code handles when a user submits a form and thus submits data to the server.
+    // In each of the below cases, when a user submits form data (a JSON object)
+    // ...the JSON is pushed to the appropriate JavaScript array.
+    // Then the server saves the data to the friendsData array)
+    // ---------------------------------------------------------------------------
+
+    app.post("/api/friends", function(req, res) {
+
+        friendsData.push(req.body);
+
+    });
+
+    //     // Create New Characters - takes in JSON input
+    // app.post("/api/new", function(req, res) {
+    //     var newcharacter = req.body;
+    //     newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+
+    //     console.log(newcharacter);
+
+    //     characters.push(newcharacter);
+
+    //     res.json(newcharacter);
+    // });
+};
