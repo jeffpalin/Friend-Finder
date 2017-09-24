13356120a2f2bc5ed771e8be1bf3719b54117b2e
Jeff Palin <jeffpalin@gmail.com>
2017-09-24 10:46:27 -0700

    udpated friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,7 +5,7 @@
 // But you could have it be an empty array as well.
 // ===============================================================================
 
-var friendsArray = {
+var friendsData = {
     "name": "Steph",
     "photo": "https://espnfivethirtyeight.files.wordpress.com/2017/04/mvp-curry-4x3.png?w=1200",
     "scores": [
@@ -23,4 +23,4 @@ var friendsArray = {
 };
 
 // Note how we export the array. This makes it accessible to other files using require.
-module.exports = friendsArray;
+module.exports = friendsData;
