6b7f22a58218c200e5d8833d9be7fc4fb5962ea5
Jeff Palin <jeffpalin@gmail.com>
2017-09-26 10:01:48 -0700

    bug fixes couldnt push last night

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,6 +6,7 @@
 
 var friendsData = require("../data/friends");
 var userData = require("../data/friends");
+var _ = require("lodash");
 
 
 // ===============================================================================
@@ -39,7 +40,34 @@ module.exports = function(app) {
     });
 
     //Compare scores of all users and pick the one with the least variance
+    // post method to check scores between users input on the form and friends in the friends array
+    app.post("/api/friends", function(req, res) {
+        var scores = req.body.scores;
+        var compareScores = [];
 
+        // each friend starts with 0 score
+        friendsData.forEach(function(friend) {
+            var friendScore = 0;
 
+            // function that calculates friendscore
+            friend.scores.forEach(function(score, num) {
+                friendScore = friendScore + Math.abs(score - parseInt(scores[num]));
+            });
 
+            // gives each friend their own score 
+            compareScores.push({
+                "friend": friend,
+                "score": friendScore
+            });
+        });
+
+        // sorts through all the scores with lodash and looks for the lowest value to the highest
+        // shift removes the first element from the array and returns it
+        res.json(_.sortBy(compareScores, "score").shift());
+    });
+
+    app.post("/api/clear", function() {
+        // Empty/clear out the arrays of data
+        friendsData = [];
+    });
 };
